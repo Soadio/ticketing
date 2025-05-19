35b7a6fc875151eb61450a960ea437a8932cd22a
Sheriff Adio <flamezee15@gmail.com>
2025-05-19 09:08:01 +0100

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ const HomePage = () => {
     <main className='flex flex-col text-center items-center justify-center min-h-screen px-4'>
       <FaTicketAlt className='mx-auto mb-4 text-red-600' size={60} />
       <h1 className='text-4xl md:text-5xl font-bold mb-4 text-blue-600'>
-        Welcome to Quick Ticket
+        Welcome to Homebase IT Support
       </h1>
       <p className='text-lg text-gray-600 mb-8'>
-        Fast and simple support ticket management system.
+        Homebase Bank IT support ticket management system.
       </p>
 
       <div className='flex flex-col md:flex-row gap-4 justify-center animate-slide opacity-0'>
